bb57b58c700677418273cf983562360160a24cb4
Tan Jun Wei <junwei2509@gmail.com>
2024-06-24 11:44:58 +0800

    Designed Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,11 +1,75 @@
-import UserItem from './userItem/UserItem'
+import UserItem from "./userItem/UserItem";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "../components/ui/command";
 
-export default function SideBar(){
+export default function SideBar() {
+  const menuList = [
+    {
+      group: "General",
+      items: [
+        {
+          link: "/",
+          text: "Home",
+        },
+        {
+          link: "/",
+          text: "Orders",
+        },
+        {
+          link: "/",
+          text: "Reports",
+        },
+      ],
+    },
+    {
+      group: "Settings",
+      items: [
+        {
+          link: "/",
+          text: "General Settings",
+        },
+        {
+          link: "/",
+          text: "Change Password",
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className="flex flex-col gap-4 w-80 border-r min-h-screen p-4">
-        <div><UserItem></UserItem></div>
-        <div className="grow">Menu</div>
-        <div>Settings</div>
+    <div className="flex flex-col gap-4 w-80 border-r min-h-screen p-3">
+      <div>
+        <UserItem></UserItem>
+      </div>
+      <div className="grow">
+        <Command className="w-full">
+          <CommandList>
+            {menuList.map((group, groupIndex) => (
+              <div key={groupIndex}>
+                <CommandGroup heading={group.group} className="pl-0 pb-2">
+                  {group.items.map((item, itemIndex) => (
+                    <a
+                      href={item.link}
+                      className="block px-2 py-2 rounded hover:bg-gray-100 text-sm"
+                    >
+                      {item.text}
+                    </a>
+                  ))}
+                </CommandGroup>
+              </div>
+            ))}
+          </CommandList>
+        </Command>
+      </div>
+      <div>Settings</div>
     </div>
-  )
+  );
 }
--- src/components/userItem/UserItem.tsx
+++ src/components/userItem/UserItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const UserItem = () => {
   return (
-    <div className="flex items-center justify-between gap-2 border rounded-l p-2">
+    <div className="flex items-center justify-between gap-2 border rounded-1 p-2 shadow-sm">
       <div
         className="avatar rounded-full min-h-10 min-w-10 bg-emerald-500 text-white font-[700]
     flex items-center justify-center"
